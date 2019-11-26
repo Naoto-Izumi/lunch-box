538e03550f96bb73e699ca897778016f0d88a0a5
haruto1 <ht18304018@ga.tera-house.ac.jp>
2019-11-26 16:01:20 +0900

    ファイルを追加しました

---
+++ layers3/WEB-INF/classes/PRegistServlet.java
@@ -0,0 +1,23 @@
+
+import JDBCFiles.RegistJdbc;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PRegistServlet extends HttpServlet{
+	protected void doGet(HttpServletRequest req,HttpServletResponse res)throws IOException,ServletException{
+		doPost(req,res);
+	}
+	public void doPost(HttpServletRequest req,HttpServletResponse res)throws IOException,ServletException{
+		req.setCharacterEncoding("Windows-31J");
+
+		String url =RegistJdbc.RegistProduct(req.getParameterMap());
+		//req.setAttribute("data",bean);
+
+		RequestDispatcher dis=req.getRequestDispatcher(url);
+		dis.forward(req,res);
+		}
+}
